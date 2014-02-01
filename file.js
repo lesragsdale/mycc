673fd51600a16578cc5a0632812d6dfebc990e93
Les <lesragsdale@gmail.com>
2014-02-01 00:31:42 -0500

    node server

--- file.js
+++ file.js
@@ -1,1 +1,6 @@
-this is just to test to make sure the repo is working like i want it to....
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:1337/');
